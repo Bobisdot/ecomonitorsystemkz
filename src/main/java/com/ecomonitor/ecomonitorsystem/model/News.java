07acd75d92cde73c4374cc4de3aba0b61bb05d0b
Bobisdot <dotisbob@gmail.com>
2025-04-01 11:48:44 +0500

    Мой первый коммит

--- src/main/java/com/ecomonitor/ecomonitorsystem/model/News.java
+++ src/main/java/com/ecomonitor/ecomonitorsystem/model/News.java
@@ -1,14 +1,39 @@
 package com.ecomonitor.ecomonitorsystem.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Entity
-@Data
+@Table(name = "news")
 public class News {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String content;
+
+    @Column(name = "created_at", nullable = true)
+    private LocalDateTime createdAt;
+
+    @Column(name = "author_id", nullable = true)
+    private Long authorId;
 }
--- src/main/java/com/ecomonitor/ecomonitorsystem/model/User.java
+++ src/main/java/com/ecomonitor/ecomonitorsystem/model/User.java
@@ -2,7 +2,11 @@ package com.ecomonitor.ecomonitorsystem.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Data
 public class User {
@@ -11,5 +15,6 @@ public class User {
     private Long id;
     private String username;
     private String email;
+    private String password;
 
 }
